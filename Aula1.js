3841f24220034e1d57de7dfffc84ca1c8bc91921
Júlio <julio.ferreira22@escola.pr.gov.br>
2026-03-09 08:54:57 -0300

    Add files via upload

---
+++ Aula1.js
@@ -0,0 +1,7 @@
+let nome = "júlio"
+let gênero = "homem"
+let idade = 15
+let roupa = "uniforme de escola"
+let acessórios = "óculos de descanso, pulseiras e aneis de prata"
+
+console.log("Olá, meu nome é " + nome + " eu sou um " + gênero + " ,eu tenho " + idade + " anos, estou usando agora " + roupa + " e também alguns acessórios como " + acessórios)
