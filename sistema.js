e09b2c4c4ff87939f37237cd81120bcf71206929
Júlio <julio.ferreira22@escola.pr.gov.br>
2026-05-06 14:03:04 -0300

    Update sistema.js

--- sistema.js
+++ sistema.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 let produtos = require("./dados");
 
 
-// mostrar produtos
+
 function mostrar() {
   console.log(produtos);
 }
 
 
-// adicionar produto
+
 function adicionar() {
   let novo = {
     id: 11,
@@ -24,14 +24,14 @@ function adicionar() {
 }
 
 
-// filtrar preço < 10
+
 function filtrar() {
   let baratos = produtos.filter(p => p.preco < 10);
   console.log(baratos);
 }
 
 
-// aplicar desconto (10%)
+
 function desconto() {
   produtos.forEach(p => {
     p.preco = p.preco * 0.9;
@@ -41,14 +41,13 @@ function desconto() {
 }
 
 
-// buscar produto
+
 function buscar() {
   let achado = produtos.find(p => p.nome === "Arroz");
   console.log(achado);
 }
 
 
-// testar
 mostrar();
 adicionar();
 filtrar();
