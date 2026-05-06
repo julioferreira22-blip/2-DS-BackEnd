f714301ec5bbe4894e1f3689273db7737ffb44bb
Júlio <julio.ferreira22@escola.pr.gov.br>
2026-05-06 14:03:25 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,8 +1,7 @@
 const fs = require("fs");
 
-// ler arquivo JSON
+
 let dados = fs.readFileSync("produtos.json");
 let produtos = JSON.parse(dados);
 
-// exportar os produtos
 module.exports = produtos;
